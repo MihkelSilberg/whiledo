92f46f33218c50048174ff42d449164251bac448
opilane <mihkel.silberg@gmail.com>
2024-05-16 12:34:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace whiledo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int num = 0;
+
+            num++;
+            Thread.Sleep(500);
+            while (num < 50)
+            {
+                Console.WriteLine(num);
+            }
+            if (num >= 50)
+            {
+                Console.WriteLine("sitarida");
+            }
+        }
+    }
+}
